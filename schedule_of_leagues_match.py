dc0149a09f4b1cc79ba13c97c72a7fa098e87d0b
Danial <daniradical69@gmail.com>
2022-08-14 10:52:14 +0430

    class base schedule league(clean code)

--- schedule_of_leagues_match.py
+++ schedule_of_leagues_match.py
@@ -1,20 +1,24 @@
 ## Import random to choose teams ##
 from random import  *
 
-class Schedule_league:
+class ScheduleLeague:
+    def __init__(self, list_of_teams):
+        self.list_of_teams = list_of_teams
+
     def random_teams(self, list_of_teams_main):
         ## Make list for append random teams ##
         list_of_selected_teams = []
 
         sub_list_teams = list_of_teams_main.copy()
 
-        ## Append random teams to list##
+
+        ## Append random teams to list ##
         for team in range(len(list_of_teams_main)):
             selected_team = choice(sub_list_teams)
             sub_list_teams.remove(selected_team)
             list_of_selected_teams.append(selected_team)
 
-        ## Devide teams to two list ##
+        ## Devide teams to two sub list ##
         sub_list_A = list_of_selected_teams[:int((len(list_of_selected_teams)/2))]
         sub_list_B = list_of_selected_teams[int((len(list_of_selected_teams)/2)):]
 
@@ -22,59 +26,62 @@ class Schedule_league:
 
 
     def games_specifications(self, list_of_teams_main):
-        number_of_all_match = ((len(list_of_teams_main))*((len(list_of_teams_main))-1))/2
         weeks_number = (len(list_of_teams_main))-1
         number_of_weeks_game = int((len(list_of_teams_main))/2)
 
         return weeks_number, number_of_weeks_game
 
 
-    def show_schedule(self, list_1, list_2, weeks_number, number_of_weeks_game):
+    def show_schedule(self, sub_list_A, sub_list_B, weeks_number, number_of_weeks_game):
         dict_of_matchs = {}
         for week in range(weeks_number):
             list_weeks_game = []
             for team in range(number_of_weeks_game):
-                first_team = list_1[team]
-                second_team = list_2[team]
+                first_team = sub_list_A[team]
+                second_team = sub_list_B[team]
                 match = 'Match {} : {} VS {}'.format(team + 1, first_team, second_team)
                 list_weeks_game.append(match)
 
-            list_1.append(list_2[-1])
-            del list_2[-1]
+            sub_list_A.append(sub_list_B[-1])
+            del sub_list_B[-1]
 
-            list_2.insert(0,list_1[1])
-            del list_1[1]
+            sub_list_B.insert(0,sub_list_A[1])
+            del sub_list_A[1]
             dict_of_matchs["Games of Week {}".format( week + 1)] = list_weeks_game
         
         return dict_of_matchs
 
 
-    def schedule(self, list = ["team_a","team_b","team_c","team_d","team_e","team_f","team_g","team_h","team_i","team_j"]):
-        # list, number_of_teams = get_list_of_teams()
-        # list_of_teams_main = get_teams_name(list, number_of_teams)
+    def schedule(self):
+        # list_of_teams, number_of_teams = get_list_of_teams()
+        # list_of_teams_main = get_teams_name(list_of_teams, number_of_teams)
 
-        if len(list) < 3:
-            return print("You can't start your league with {} team.".format(len(list))) 
+        if len(self.list_of_teams) < 3:
+            print("You can't start your league with {} team. You need more than 2 teams.".format(len(self.list_of_teams))) 
+            return
 
-        for team_name in list:
+        for team_name in self.list_of_teams:
             if type(team_name) != str:
-                return print("Your teams name must be a  <class 'string'> not {}.".format(type(team_name)))
+                print("Your teams name must be a  <class 'string'> not {}.".format(type(team_name)))
+                return
 
-            if list.count(team_name.replace(" ","")) > 1:
-                return print("There are more than one team with name '{}' in the team list.".format(team_name))
-            
+            if self.list_of_teams.count(team_name.replace(" ","")) > 1:
+                print("There are more than one team with name '{}' in the team list.".format(team_name))
+                return
+
             if len(team_name.replace(' ', '')) == 0:
-                return print('There are none value in team list.')
+                print('There are none value in team list.')
+                return
 
-        if len(list) >= 3:
-            print("Your league have {} teams.".format(len(list)))
+        if len(self.list_of_teams) >= 3:
+            print("Your league have {} teams.".format(len(self.list_of_teams)))
 
-        if len(list) % 2 != 0 :
-                list.append('Break')
+        if len(self.list_of_teams) % 2 != 0 :
+                self.list_of_teams.append('Break')
 
-        sub_list_A, sub_list_B = self.random_teams(list)
+        sub_list_A, sub_list_B = self.random_teams(self.list_of_teams)
 
-        weeks_number, number_of_weeks_game = self.games_specifications(list)
+        weeks_number, number_of_weeks_game = self.games_specifications(self.list_of_teams)
 
         matchs = self.show_schedule(sub_list_A, sub_list_B, weeks_number, number_of_weeks_game)
 
